1ddfe1c033b567f44a29be12004b33ac7bd6c420
Matheus Henrique de Souza Fontenele <matheus.henriquefont@gmail.com>
2021-10-16 18:47:01 -0300

    fix: main.dart works

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:obifilmes/app/routes/app_pages.dart';
 import 'package:obifilmes/app/routes/app_routes.dart';
 import 'package:obifilmes/app/ui/themes/themes.dart';
 
 void main() async {
-  await GetStorage.init();
   runApp(GetMaterialApp(
     title: 'ObiFilmes',
     theme: Themes.appLightTheme,
